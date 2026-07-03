7d9c6f84d7d072621b0b4ca3ae3552cbdacd4696
RafaelRaposoDeQuadros <rafaelraposodequadros@gmail.com>
2026-07-03 14:00:55 -0300

    insert implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -31,5 +31,10 @@ public class Program {
         for(Seller obj : list){
             System.out.println(obj);
         }
+
+        System.out.println("\n === TESTE 4: seller insert===");
+        Seller newSeller = new Seller(null, "Greg","greg@gmail.com",new Date(),40000.0,department);
+        sellerDao.insert(newSeller);
+        System.out.println("Insert! New Id= " + newSeller.getId());
     }
 }
